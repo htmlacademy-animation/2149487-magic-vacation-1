4084f0fab91858fa7db5b050679882ed0b7e0dec
danil-rezbaev <92051977+danil-rezbaev@users.noreply.github.com>
2022-09-07 02:32:39 +0300

    module2-task2

--- source/js/modules/animations.js
+++ source/js/modules/animations.js
@@ -1,28 +1,60 @@
 export default () => {
-  // task 1
-  function task1() {
-    const fadeInElements = [...document.querySelectorAll(`.fadein`)];
-
-    const onIntersect = (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add(`fadein-active`);
-        } else {
-          entry.target.classList.remove(`fadein-active`);
-        }
-      });
-    };
-
-    const observer = new IntersectionObserver(onIntersect);
-
-    fadeInElements.forEach((item) => {
-      observer.observe(item);
-    });
-  }
-
-  // task 2
+  // module 1 task 2
 
   window.addEventListener(`load`, () => {
     document.body.classList.add(`loaded`);
+  });
+
+  // module 2 task 2
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  function letterWrap(item, tag, className, ...options) {
+    const wrapper = document.createElement(tag);
+    wrapper.classList = `letters-animation--letter ${options.join(` `)}`;
+    wrapper.innerHTML = item;
+
+    return wrapper;
+  }
+
+  function wordWrap(item, tag, ...options) {
+    const wrapper = document.createElement(tag);
+    wrapper.classList = `letters-animation--word ${options}`;
+
+    item.split(``).forEach((word) => (
+      wrapper.appendChild(letterWrap(word, `span`, `letters-animation--letter`, `delay-${getRandomInt(1, 10) * 35}ms`, `duration-${getRandomInt(1, 10) * 100}ms`))
+    ));
+
+    return wrapper;
+  }
+
+  function stringWrap(item, tag, ...options) {
+    const wrapper = document.createElement(tag);
+    wrapper.classList = `letters-animation--string ${options}`;
+
+    item.split(` `).forEach((word) => (
+      wrapper.appendChild(wordWrap(word, `span`))
+    ));
+
+    return wrapper;
+  }
+
+  function lettersTextAnimation(text) {
+    let innerHTML = text.innerHTML;
+    text.innerHTML = ``;
+
+    innerHTML.split(`<br>`).map((word) => (
+      text.appendChild(stringWrap(word, `span`))
+    ));
+  }
+
+  let animationElements = [...document.querySelectorAll(`.letters-animation`)];
+
+  animationElements.forEach((item) => {
+    lettersTextAnimation(item);
   });
 };
